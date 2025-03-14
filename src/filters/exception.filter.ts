ac9a1592ca5c062985b159026f3d85dba59e31d4
Soveha <vatsoveha07@gmail.com>
2025-03-14 16:46:51 +0700

    Refactor exception handling and response structure to use StatusCodes enum for improved readability and maintainability

--- src/filters/exception.filter.ts
+++ src/filters/exception.filter.ts
@@ -1,5 +1,7 @@
 import { ExceptionFilter, Catch, ArgumentsHost, HttpException, InternalServerErrorException } from '@nestjs/common';
 import { Response } from 'express';
+import { stat } from 'fs';
+import { StatusCodes } from 'src/enums/statusCodes';
 import { QueryFailedError } from 'typeorm';
 
 @Catch()
@@ -8,7 +10,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
         const ctx = host.switchToHttp();
         const response = ctx.getResponse<Response>();
 
-        let status = 500;
+        let status = StatusCodes.InternalServerError; // Internal Server Error
         let message = 'Internal server error';
 
         // If the error is an instance of HttpException, use its status and message
@@ -19,12 +21,12 @@ export class GlobalExceptionFilter implements ExceptionFilter {
         }
         // If the error is a QueryFailedError from TypeORM, return a proper error message
         else if (exception instanceof QueryFailedError) {
-            status = 400; // Bad Request
+            status = StatusCodes.BadRequest; // Bad Request
             message = (exception as any).message || 'Database query failed';
         }
 
         response.status(status).json({
-            status: 'error',
+            status: status,
             timestamp: new Date(new Date().toISOString()).toLocaleString(),
             message: typeof message === 'string' ? message : (message as any).message || 'An error occurred',
         });
--- src/interceptors/response.interceptor.ts
+++ src/interceptors/response.interceptor.ts
@@ -5,13 +5,14 @@ import {
     NestInterceptor
 } from '@nestjs/common';
 import { Observable, map } from 'rxjs';
+import { StatusCodes } from 'src/enums/statusCodes';
 
 @Injectable()
 export class ResponseInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
         return next.handle().pipe(
             map((data) => ({
-                status: 'success',
+                status: StatusCodes.Success,
                 timestamp: new Date(new Date().toISOString()).toLocaleString(),
                 data: data
             }))
